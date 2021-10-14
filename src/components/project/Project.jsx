c3169a550b23edd323756d5f8497e2bc2926b469
nehaahuja2021 <nehaahuja2021@gmail.com>
2021-10-14 10:47:23 +0200

    recent changes

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -2,15 +2,18 @@ import "./project.css"
 
 export default function Project() {
 const projectDInfo =[
-{img:"/images/corona.png",pname:"Corona Worldometer",pdetails:"Html,Css,JS",pdescription:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio, obcaecati velit totam deleniti numquam pariatur corporis delectus nesciunt neque, voluptatibus tempore unde sequi, consectetur sunt exercitationem at beatae. A, dolores? Et perspiciatis recusandae animi!",link:"https://nehaahuja2021.github.io/CoronaWorldometer.github.io/" },
-{img:"/images/cars1.jpg",pname:"Best Car Deals",pdetails:"JS,NodeJs,Sqlite",pdescription:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio, obcaecati velit totam deleniti numquam pariatur corporis delectus nesciunt neque, voluptatibus tempore unde sequi, consectetur sunt exercitationem at beatae. A, dolores? Et perspiciatis recusandae animi!"},
-{img:"/images/farm.jpg",pname:"FreshShop",pdetails:"PHP,Laravel,Bootstrap,SQL",pdescription:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio, obcaecati velit totam deleniti numquam pariatur corporis delectus nesciunt neque, voluptatibus tempore unde sequi, consectetur sunt exercitationem at beatae. A, dolores? Et perspiciatis recusandae animi!"},
-{img:"/images/textimage.png",pname:"TextApp in React",pdetails:"React-Js,Bootstrap",pdescription:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio, obcaecati velit totam deleniti numquam pariatur corporis delectus nesciunt neque, voluptatibus tempore unde sequi, consectetur sunt exercitationem at beatae. A, dolores? Et perspiciatis recusandae animi!"}
+
+{img:"/images/cars1.jpg",pname:"Best Car Deals",pdetails:"JS,NodeJs,Sqlite,Express,Heroku",pdescription:"Features : (1) This small app about car deals helps you to find cars of brands ( Audi,BMW,Chevrolet,Alpharomeo,Lambhorgini ) from budget (starting range :20000)Euros. (2) User can use contact us form and all user details can be saved in an excel sheet at backend.",link: "https://appbestcardeal.herokuapp.com/"},
+{img:"/images/farm.jpg",pname:"FreshShop",pdetails:"Laravel,Bootstrap,MYSQL,Heroku",pdescription:"Features: Home Page with links like : Products Available, Search Any Product, Register and Login.User can Register & after registration, lands to login page.From Home Page, User can add products only if he is logged in.User can view his orders,add & delete products from cart.User can place an order only with Payment on Delivery Option. Address needs to be filled.",link:" http://appfreshfood.herokuapp.com/"},
+{img:"/images/textimage.png",pname:"TextApp in React",pdetails:"React-Js,Bootstrap",pdescription:" Features:Built using React functional components and hooks, this app converts Text to LowerCase, UpperCase, Copies Text, Deletes Text,Enables Dark or Light Modes and is Responsive",link:"https://apptext-4u.herokuapp.com/"},
+{img:"/images/corona.png",pname:"Corona Worldometer",pdetails:"Html,Css,JS",pdescription:"Features: This app was built using API to display Corona Cases in almost 200 countries.User needs to input country name and it displays all information related to cases in that country.",link:"https://nehaahuja2021.github.io/CoronaWorldometer.github.io/" }
 ]
 
 const renderProject=(project,index)=>{
+   
 return(
    <>
+  
          <img className="projectImg" src={project.img} alt=""/>
          <div className="projectInfo">
             <div className="projectName">{project.pname}</div>
@@ -18,7 +21,7 @@ return(
             <span className="projectDesc">
             <p>{project.pdescription} </p>
          </span>
-         <a href={project.link} class="projectBtn">Read More </a>
+         <a className="-btn -bg -txt" href={project.link} >Click Here to View </a>
          </div>
 </>
 )}
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,7 +4,9 @@ import  "./projects.css"
 export default function Projects() {
    return (
       <div className="projects">
-         
+          
+       <p className="pmainbar"> A Selection of Stuff I've Built</p>
+       
          <Project/>
          </div>
          
